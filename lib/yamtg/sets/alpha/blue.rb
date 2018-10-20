10440a6944bc711638a7e7e971fa885f3a56178e
Vasiliy Yeremeyev <vayerx@gmail.com>
2018-10-21 00:58:20 +0300

    drop yamtg header

--- lib/yamtg/sets/alpha/blue.rb
+++ lib/yamtg/sets/alpha/blue.rb
@@ -1,33 +1,3 @@
-#############################################################################
-#    Yet Another 'Magic: The Gathering' Game Simulator                      #
-#    (C) 2009-2011, Vasiliy Yeremeyev <vayerx@gmail.com>,                   #
-#    (C) 2007, Stefan Rusterholz (rubyoh).                                  #
-#                                                                           #
-#    This program is free software; you can redistribute it and/or modify   #
-#    it under the terms of the GNU General Public License as published by   #
-#    the Free Software Foundation; either version 2 of the License, or      #
-#    (at your option) any later version.                                    #
-#                                                                           #
-#    This program is distributed in the hope that it will be useful,        #
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of         #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the           #
-#    GNU General Public License for more details.                           #
-#                                                                           #
-#    You should have received a copy of the GNU General Public License      #
-#    along with this program. If not, see <http://www.gnu.org/licenses/>    #
-#    or write to the Free Software Foundation, Inc.,                        #
-#    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.              #
-#############################################################################
-
-# blue typically does not have the best creatures, because normally
-# the blue spells, and counterspells control the game.
-# blue does however have some fliers and some other good creatures
-# like creatures with "unblockable" sneak through enemy lines or
-# draw-card abilities, and of course it has some key "avatar" cards
-# for late game just like the other colours too.
-
-# Blue flier. Should be in spirit like the black bat, and the
-# white pegasus, but its a bit more expensive.
 creature 'Zephyr of the sky' do
     cost        1.blue, 2.colorless
     type        :Bird   # or Animals/Bird or just Bird?
